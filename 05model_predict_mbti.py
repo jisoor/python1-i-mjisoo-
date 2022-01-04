99fb01bdf7784f2decd6b62e770ae623039829bc
jisoor <jisoo0525@hanmail.net>
2022-01-05 00:48:20 +0900

    all mbti crawling- modeling

--- 05model_predict_mbti.py
+++ 05model_predict_mbti.py
@@ -10,13 +10,10 @@ import pickle
 from tensorflow.keras.models import load_model
 
 pd.set_option('display.unicode.east_asian_width', True)
-# data load
-df = pd.read_csv('./crawling/naver_headline_news20211116.csv')
-print(df.head())
-print(df.info())
 
-X = df['title']
-Y = df['category']
+
+X = 'jisoo is so kind and generous , passionate about her life'
+Y = ['enfp','estp', 'esfp', 'entp', 'estj', 'esfj', 'enfj', 'entj', 'istj', 'isfj', 'infj', 'intj', 'istp', 'isfp', 'infp', 'intp']
 
 #target labeling
 with open('./models/encoder.pickle', 'rb') as f:
@@ -30,27 +27,65 @@ print(label)
 onehot_Y = to_categorical(labeled_Y)
 print(onehot_Y)
 # 형태소 분리, 한 글자/불용어 제거
-okt = Okt()
-for i in range(len(X)):
-    X[i] = okt.morphs(X[i], stem=True)
+import nltk
+from nltk.tag import untag
+from nltk.stem import WordNetLemmatizer
 
-stopwords = pd.read_csv(
-    './crawling/stopwords.csv',
-    index_col=0)
+# ############# tokenizing
+tokened_X = nltk.word_tokenize(X)
+print(tokened_X)
 
-for j in range(len(X)):
+############# pos 태깅 ( word_tokenize 먼저 해야함)
+possed_X = nltk.pos_tag(tokened_X)
+print(possed_X)
+
+## lemmatzing  품사가 VB VBD VBG VBN VBP VBZ 애네만 골라서 동사 원형(lemmatizing)만들기
+lm = WordNetLemmatizer()
+for i in range(len(possed_X)):
+    possed_X[i] = list(possed_X[i])
+    if (possed_X[i][1] == "VB") | (possed_X[i][1] == "VBD") | (possed_X[i][1] == "VBG")| (possed_X[i][1] == "VBN")| (possed_X[i][1] == "VBP")| (possed_X[i][1] == "VBZ"):
+        possed_X[i][0] = lm.lemmatize(possed_X[i], pos = 'v')
+
+print(possed_X)
+
+# 모두 untag 해서 리스트 만들기
+for i in range(len(possed_X)):
+    possed_X[i] = possed_X[i][0]
+
+print('untagged\lists: ', possed_X )
+
+# 한글자인 단어 빼기, stopwords에 관형사 빼기
+# 그 다음 tokenizing 시작
+
+
+#  불용어, 한글자제거
+stopwords = pd.read_csv('./stopwords(Eng).csv')
+stopwords = stopwords.T
+stopwords.reset_index(inplace=True)
+print(stopwords.info())
+print(stopwords.columns)
+
+# for j in range(len(X)):
+#     words=[]
+#     for i in range(len(X[j])):
+#         if len(X[j][i]) > 1:
+#             if X[j][i] not in list(stopwords['index']):
+#                 words.append(X[j][i])
+#     X[j] = ' '.join(words)
+
+for i in range(len(possed_X)):
     words = []
-    for i in range(len(X[j])):
-        if len(X[j][i]) > 1:
-            if X[j][i] not in list(stopwords['stopword']):
-                words.append(X[j][i])
-    X[j] = ' '.join(words)
-print(X)
+    if len(possed_X[i]) > 1:
+        if possed_X[i] not in list(stopwords['index']):
+            words.append(possed_X[i])
+X = ' '.join(words)
+print('불용어 까지 제거한 X', X)
+
 
 
 # titles tokenizing
 
-with open('./models/news_token.pickle', 'rb') as f:
+with open('./models/mbti_token.pickle', 'rb') as f:
     token = pickle.load(f)
 
 tokened_X = token.texts_to_sequences(X)
@@ -58,18 +93,19 @@ print(tokened_X[:5])
 
 
 for i in range(len(tokened_X)):
-    if 23 < len(tokened_X[i]):
-        tokened_X[i] = tokened_X[i][:23]
+    if 1250 < len(tokened_X[i]):
+        tokened_X[i] = tokened_X[i][:1250]
 # padding
-X_pad = pad_sequences(tokened_X, 23)
+X_pad = pad_sequences(tokened_X, 1250)
 print(X_pad[:10])
 
 # model.load
 # model.predict(X_pad)
 # predict과 onehot_Y와 비교
 
+############### d여기까지함 ##########################
 
-model = load_model('./models/news_category_classfication_model_0.7447780966758728.h5')
+model = load_model('./models/mbti_classification_model_0.2888889014720917.h5')
 preds = model.predict(X_pad)
 predicts = []
 for pred in preds:
--- preprocessing03_mbti.py
+++ preprocessing03_mbti.py
@@ -1,5 +1,4 @@
 import pandas as pd
-<<<<<<< HEAD
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -7,21 +6,15 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
 import pickle
-
-# y값전처리, x값 nltk랑 불용어 제거, x값 전처리 encoder, token, modeling npy 만들기
-
-pd.set_option('display.unicode.east_asian_width', True)
-df = pd.read_csv('./crawling/recompiled_all_mbti.csv')
-=======
 from sklearn.preprocessing import LabelEncoder
 import pickle
 from tensorflow.keras.utils import to_categorical
 from konlpy.tag import Okt # 가 아니라 영문 nltk
 
+# y값전처리, x값 nltk랑 불용어 제거, x값 전처리 encoder, token, modeling npy 만들기
 
 pd.set_option('display.unicode.east_asian_width', True)
-df = pd.read_csv('./crawling/final_all_mbti.csv')
->>>>>>> origin/main
+df = pd.read_csv('./crawling/recompiled_all_mbti.csv')
 print(df.head())
 print(df.info())
 
@@ -35,7 +28,7 @@ labeled_Y = encoder.fit_transform(Y) # enfp, estj 등의 타입을 숫자로 0~1
 label = encoder.classes_
 print(labeled_Y[:5])
 print(label)  # encoder라벨정보가 리스트로 나옴
-<<<<<<< HEAD
+
 # with open('./models/encoder.pickle', 'wb') as f:
 #     pickle.dump(encoder, f)
 onehot_Y = to_categorical(labeled_Y) # 행렬로 만듬
@@ -88,32 +81,11 @@ print(stopwords.info())
 print(stopwords.columns)
 
 # exit()
-=======
-with open('./models/encoder.pickle', 'wb') as f:
-    pickle.dump(encoder, f)
-onehot_Y = to_categorical(labeled_Y) # 행렬로 만듬
-print(onehot_Y)
 
-########## 형태소 분리
-okt = Okt()
-print(type(X))
-okt_morph_X = okt.morphs(X[10], stem=True)
-print(X[10])
-print(okt_morph_X)
-
-
-for i in range(len(X)):
-    X[i] = okt.morphs(X[i])
-# print(X)
-
-stopwords = pd.read_csv('./crawling/stopwords.csv', index_col=0)
-
->>>>>>> origin/main
 for j in range(len(X)):
     words=[]
     for i in range(len(X[j])):
         if len(X[j][i]) > 1:
-<<<<<<< HEAD
             if X[j][i] not in list(stopwords['index']):
                 words.append(X[j][i])
     X[j] = ' '.join(words)
@@ -124,25 +96,15 @@ print('불용어 까지 제거한 X', X)
 # print(cleaned_df.head())
 
 ########### 토크나이징
-=======
-            if X[j][i] not in list(stopwords['stopword']):
-                words.append(X[j][i])
-    X[j] = ' '.join(words)
-print(X)
 
->>>>>>> origin/main
 token = Tokenizer()
 token.fit_on_texts(X)
 tokened_X = token.texts_to_sequences(X)
 print(tokened_X[:5])
 
-<<<<<<< HEAD
 # with open('./models/mbti_token.pickle', 'wb') as f:
 #     pickle.dump(token, f)
-=======
-with open('./models/news_token.pickle', 'wb') as f:
-    pickle.dump(token, f)
->>>>>>> origin/main
+
 
 wordsize = len(token.word_index) + 1
 print(wordsize)
@@ -157,18 +119,11 @@ print(max)
 X_pad = pad_sequences(tokened_X, max)
 print(X_pad[:10])
 
-<<<<<<< HEAD
+
 # X_train, X_test, Y_train, Y_test = train_test_split(X_pad, onehot_Y, test_size=0.1)
 # print(X_train.shape, Y_train.shape)
 # print(X_test.shape, Y_test.shape)
 #
 # xy = X_train, X_test, Y_train, Y_test
 # np.save('./crawling/mbti_data_max_{}_wordsize_{}'.format(max, wordsize), xy)
-=======
-X_train, X_test, Y_train, Y_test = train_test_split(X_pad, onehot_Y, test_size=0.1)
-print(X_train.shape, Y_train.shape)
-print(X_test.shape, Y_test.shape)
 
-xy = X_train, X_test, Y_train, Y_test
-np.save('./crawling/news_data_max_{}_wordsize_{}'.format(max, wordsize), xy)
->>>>>>> origin/main
